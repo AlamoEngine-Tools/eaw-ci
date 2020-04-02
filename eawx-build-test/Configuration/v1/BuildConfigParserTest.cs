b7d993ac66b1703458414bc15726cbd802656655
KadVenku <venkukad@gmail.com>
2020-04-02 18:08:36 +0200

    Updates the XSD Schema

---
+++ eawx-build-test/Configuration/v1/BuildConfigParserTest.cs
@@ -0,0 +1,146 @@
+using System;
+using System.IO.Abstractions.TestingHelpers;
+using System.Xml.Schema;
+using EawXBuild.Configuration.v1;
+using EawXBuild.Core;
+using EawXBuildTest.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EawXBuildTest.Configuration.v1
+{
+    [TestClass]
+    public class BuildConfigParserTest
+    {
+        private const string Path = "MyXml.xml";
+
+        private MockFileSystem _fileSystem;
+        private MockFileData _mockFileData;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mockFileData = new MockFileData(string.Empty);
+            _fileSystem = new MockFileSystem();
+            _fileSystem.AddFile(Path, _mockFileData);
+        }
+
+        [TestMethod]
+        public void
+          GivenXmlWithWrongConfigVersion__WhenCallingParse__ShouldThrowException()
+        {
+          const string xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                                <eaw-ci:BuildConfiguration
+                                  ConfigVersion=""1.0.1"" 
+                                  xmlns:eaw-ci=""eaw-ci""
+                                  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+                                  <Projects>
+                                    <Project Id=""idvalue0"" Name=""TestProject"">
+                                      <Jobs>
+                                        <Job Id=""idvalue1"">
+                                          <Tasks>
+                                            <Task Id=""idvalue2"" xsi:type=""eaw-ci:AbstractTaskType""/>
+                                          </Tasks>
+                                        </Job>
+                                      </Jobs>
+                                    </Project>
+                                  </Projects>
+                                </eaw-ci:BuildConfiguration>";
+
+          _mockFileData.TextContents = xml;
+          const string exceptionMessage = "The value of the 'ConfigVersion' attribute does not equal its fixed value.";
+          var factoryStub = new BuildComponentFactoryStub {Project = new ProjectStub()};
+          var sut = new BuildConfigParser(_fileSystem, factoryStub);
+          try
+          {
+            sut.Parse(Path);
+          }
+          catch (Exception e)
+          {
+            Assert.AreEqual(typeof(InvalidOperationException), e.GetType());
+            Assert.AreEqual(typeof(XmlSchemaValidationException), e.InnerException.GetType());
+            Assert.AreEqual(exceptionMessage, e.InnerException.Message);
+          }
+        }
+
+        [TestMethod]
+        public void GivenXmlWithSingleProject__WhenCallingParse__ShouldReturnArrayWithMatchingProject()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                                <eaw-ci:BuildConfiguration
+                                  ConfigVersion=""1.0.0"" 
+                                  xmlns:eaw-ci=""eaw-ci""
+                                  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+                                  <Projects>
+                                    <Project Id=""idvalue0"" Name=""TestProject"">
+                                      <Jobs>
+                                        <Job Id=""idvalue1"">
+                                          <Tasks>
+                                            <Task Id=""idvalue2"" xsi:type=""eaw-ci:AbstractTaskType""/>
+                                          </Tasks>
+                                        </Job>
+                                      </Jobs>
+                                    </Project>
+                                  </Projects>
+                                </eaw-ci:BuildConfiguration>";
+
+            _mockFileData.TextContents = xml;
+
+            const string projectName = "TestProject";
+
+            var factoryStub = new BuildComponentFactoryStub {Project = new ProjectStub()};
+            var sut = new BuildConfigParser(_fileSystem, factoryStub);
+
+            var projects = sut.Parse(Path);
+
+            var actual = projects[0] as ProjectStub;
+            var actualName = actual?.Name;
+            AssertProjectNameEquals(projectName, actualName);
+        }
+
+        [TestMethod]
+        public void GivenXmlWithSingleProjectAndJob__WhenCallingParse__ProjectShouldHaveMatchingJob()
+        {
+          const string xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                                <eaw-ci:BuildConfiguration
+                                  ConfigVersion=""1.0.0"" 
+                                  xmlns:eaw-ci=""eaw-ci""
+                                  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+                                  <Projects>
+                                    <Project Id=""idvalue0"">
+                                      <Jobs>
+                                        <Job Id=""idvalue1"" Name=""TestJob"">
+                                          <Tasks>
+                                            <Task Id=""idvalue2"" xsi:type=""eaw-ci:AbstractTaskType""/>
+                                          </Tasks>
+                                        </Job>
+                                      </Jobs>
+                                    </Project>
+                                  </Projects>
+                                </eaw-ci:BuildConfiguration>";
+
+            _mockFileData.TextContents = xml;
+
+            const string jobName = "TestJob";
+
+            var factoryStub = new BuildComponentFactoryStub {Project = new ProjectStub(), Job = new JobStub()};
+            var sut = new BuildConfigParser(_fileSystem, factoryStub);
+
+            var projects = sut.Parse(Path);
+
+            var actualProject = projects[0] as ProjectStub;
+            var actualJob = actualProject.Jobs[0];
+            AssertJobNameEquals(jobName, actualJob);
+        }
+
+        private static void AssertProjectNameEquals(string projectName, string actualName)
+        {
+            Assert.AreEqual(projectName, actualName,
+                $"Should return Project with name {projectName}, but was {actualName}");
+        }
+
+        private static void AssertJobNameEquals(string jobName, IJob actualJob)
+        {
+            Assert.AreEqual(jobName, actualJob.Name, $"Job name should be {jobName}, but was {actualJob.Name}");
+        }
+    }
+}
