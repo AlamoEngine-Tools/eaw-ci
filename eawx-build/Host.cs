b5a5c89931c4e19378688a9dada97e81f631f114
Lukas Grünwald <gruenwald.lk@outlook.com>
2020-06-07 16:12:08 +0200

    [FEAT(#13)] Command Line Parser
    The tool now takes two command line arguments:
      * -c/--config  A (relative) path to a configuration file.
      * -v/--verbose A debug option to enable full logging output.
    
    Closes #13

---
+++ eawx-build/Host.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using CommandLine;
+using EawXBuild.Configuration.CI;
+using EawXBuild.Environment;
+using EawXBuild.Services.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace EawXBuild
+{
+    internal class Host
+    {
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(ExecInternal)
+                .WithNotParsed(HandleParseErrorsInternal);
+        }
+
+        private static void ExecInternal(Options opts)
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection, opts.Verbose);
+            EawXBuildApplication application = new EawXBuildApplication(serviceCollection.BuildServiceProvider(), opts);
+            System.Environment.ExitCode = (int) application.Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection, bool verbose)
+        {
+            serviceCollection.AddLogging(config =>
+                {
+                    config.AddDebug();
+                    config.AddConsole();
+                })
+                .Configure<LoggerFilterOptions>(options =>
+                {
+                    options.AddFilter<DebugLoggerProvider>(null, LogLevel.Trace);
+                    options.AddFilter<ConsoleLoggerProvider>(null, verbose ? LogLevel.Information : LogLevel.Warning);
+                });
+            ServiceProvider lsp = serviceCollection.BuildServiceProvider();
+            serviceCollection.AddTransient<IIOService, IOService>(s =>
+                new IOService(new FileSystem(), lsp.GetRequiredService<ILoggerFactory>().CreateLogger<IOService>()));
+        }
+
+        private static void HandleParseErrorsInternal(IEnumerable<Error> errs)
+        {
+            System.Environment.ExitCode = (int) ExitCode.ExUsage;
+        }
+    }
+}
