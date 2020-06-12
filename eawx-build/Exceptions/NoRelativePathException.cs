2e7cc6a3ee1db29ab3bb40d50291d508f4a40f8b
Sven Marcus <s.marcus@outlook.de>
2020-06-12 23:30:45 +0200

    Serializable and reformatted Exceptions

--- eawx-build/Exceptions/NoRelativePathException.cs
+++ eawx-build/Exceptions/NoRelativePathException.cs
@@ -1,6 +1,7 @@
 using System;
 
 namespace EawXBuild.Exceptions {
+    [Serializable]
     public class NoRelativePathException : Exception {
         public NoRelativePathException(string path) : base($"Path {path} is not a relative path") {
         }
