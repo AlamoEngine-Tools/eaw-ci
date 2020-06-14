a45baf055e41b570e35f5a3080585756aad3350f
Sven Marcus <s.marcus@outlook.de>
2020-06-14 16:26:48 +0200

    Initial BuildComponentFactory + CopyTaskBuilder

---
+++ eawx-build/Configuration/v1/CopyTaskBuilder.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO.Abstractions;
+using EawXBuild.Core;
+using EawXBuild.Tasks;
+
+namespace EawXBuild.Configuration.v1 {
+    public class CopyTaskBuilder : ITaskBuilder {
+        private readonly CopyTask _copyTask = new CopyTask(new FileSystem());
+
+        public ITaskBuilder With(string name, object value) {
+            switch (name) {
+                case "CopyFromPath":
+                    _copyTask.Source = (string) value;
+                    break;
+                case "CopyToPath":
+                    _copyTask.Destination = (string) value;
+                    break;
+                case "CopySubfolders":
+                    _copyTask.Recursive = (bool) value;
+                    break;
+                case "CopyFileByPattern":
+                    _copyTask.FilePattern = (string) value;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Invalid configuration option: {name}");
+            }
+            return this;
+        }
+
+        public ITask Build() {
+            return _copyTask;
+        }
+    }
+}
