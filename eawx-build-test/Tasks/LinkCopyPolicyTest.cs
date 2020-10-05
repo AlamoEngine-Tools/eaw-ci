454f0281382250abd4c8f020a47be88fd0008fb1
Sven Marcus <s.marcus@outlook.de>
2020-10-05 12:45:19 +0200

    Use platform specific paths for LinkCopyPolicyTest

--- eawx-build-test/Tasks/LinkCopyPolicyTest.cs
+++ eawx-build-test/Tasks/LinkCopyPolicyTest.cs
@@ -7,17 +7,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace EawXBuildTest.Tasks {
     [TestClass]
     public class LinkCopyPolicyTest {
+        private string _currentDir;
         private string _sourceFileName;
         private string _targetFileName;
         private MockFileSystem _fileSystem;
 
         [TestInitialize]
         public void SetUp() {
-            _sourceFileName = "/home/folder/sourceFile";
-            _targetFileName = "/home/folder/targetFile";
+            SetPlatformSpecificPaths();
             SetUpFileSystem();
         }
-        
+
         [TestMethod]
         public void GivenSourceAndTargetFileInfo__WhenCopyTo__ShouldCallLinkerWithFullFileNames() {
             var sourceFile = _fileSystem.FileInfo.FromFileName(_sourceFileName);
@@ -27,7 +27,7 @@ namespace EawXBuildTest.Tasks {
             var sut = new LinkCopyPolicy(fileLinkerSpy);
 
             sut.CopyTo(sourceFile, targetFile, true);
-            
+
             Assert.AreEqual(_sourceFileName, fileLinkerSpy.ReceivedSource);
             Assert.AreEqual(_targetFileName, fileLinkerSpy.ReceivedTarget);
         }
@@ -45,14 +45,25 @@ namespace EawXBuildTest.Tasks {
             Assert.IsFalse(fileLinkerSpy.CreateLinkWasCalled);
         }
 
-        private void SetUpFileSystem() {
-            var currentDir = "/home/folder";
+        private void SetPlatformSpecificPaths() {
+            if (TestUtility.IsWindows()) {
+                _currentDir = @"C:\folder";
+                _sourceFileName = @"C:\folder\sourceFile";
+                _targetFileName = @"C:\home\folder\targetFile";
+                return;
+            }
 
+            _currentDir = "/home/folder";
+            _sourceFileName = "/home/folder/sourceFile";
+            _targetFileName = "/home/folder/targetFile";
+        }
+
+        private void SetUpFileSystem() {
             var files = new Dictionary<string, MockFileData> {
                 {_sourceFileName, string.Empty},
                 {_targetFileName, string.Empty}
             };
-            _fileSystem = new MockFileSystem(files, currentDir);
+            _fileSystem = new MockFileSystem(files, _currentDir);
         }
     }
 }
